70cb1f3d7f02a057f03f38a4f790b22d0be6a60c
Paul Jones <paul.tyler.jones@gmail.com>
2025-10-24 07:58:43 +0000

    Adds comparison operations

--- python-script/python-script.py
+++ python-script/python-script.py
@@ -2,5 +2,7 @@
 # 2025-10-24
 # Operations
 print("Hello, World!")
-print(2 + 2)
-print(10 / 5)
+print(2 + 2) # addition
+print(10 / 5) # division
+print(25 > 20) # comparison - greater than
+print (15 == 15) # comparison - value
