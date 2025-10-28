a42075fadbbc9ba011e10b7c77c4e53cd2382f5d
Gg630925 <155528841+Gg630925@users.noreply.github.com>
2025-10-28 07:50:20 +0000

    git commit -m "Adds math Operations"

---
+++ python-script/python-script.py
@@ -0,0 +1,13 @@
+# Super
+# 2025-10-28
+# Operations
+print("Hello, World!")
+print(2+2)
+print(10/5)
+print(25>20)
+print(15==15)
+# height = 5.9
+# Variables with our information
+student_name = "Super"
+student_age = 18
+favorite_number = 14
