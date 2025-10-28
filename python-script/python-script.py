4ef3d31b12d46e3dc42c8bd19f82e22d81fcd7f0
Gg630925 <155528841+Gg630925@users.noreply.github.com>
2025-10-28 08:09:21 +0000

    creates sum and difference

--- python-script/python-script.py
+++ python-script/python-script.py
@@ -2,16 +2,24 @@
 # 2025-10-28
 # Operations
 print("Hello, World!")
-print(2+2)
-print(10/5)
-print(25>20)
-print(15==15)
+print(2+2) # 
+print(10/5) # 
+print(25>20) # comparison - greater than
+print(15==15) # comparison - value
 # height = 5.9
 # Variables with our information
 student_name = "Super"
 student_age = 18
 favorite_number = 14
-#Print each varibale
+# Print each varibale
 print(student_name)
 print(student_age)
 print(favorite_number)
+# perform calculations with variables
+num1 = 10
+num2 = 5
+# calculations with variables
+sum_result = num1 + num2
+different = num1 - num2
+print(sum_result)
+print(different)
